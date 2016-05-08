8e0b449729a466a527a0129142fabaa89873811c
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-08 14:05:09 +0300

    Pass image in base64 instead of url

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
-function sendMsg(info, tab) {
+function imageUpload(img) {
+  let imgTag = document.createElement('img');
+  imgTag.onload = function() {
+    let width = imgTag.width;
+    let height = imgTag.height;
+
+    let canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+
+    let ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, width, height);
+    ctx.drawImage(imgTag, 0, 0, width, height);
+
+    var base64Img = canvas.toDataURL('image/png', 1);
+    imgTag.src = "";
+
+    sendMsg(base64Img);
+  };
+  imgTag.src = img;
+}
+
+function getImg(url) {
+  if (0 == url.indexOf('data:')) {
+    return url;
+  } else {
+    return imageUpload(url);
+  }
+}
+
+function sendMsg(imgData) {
+  let x = imgData.split(',')[1].toString();
+  console.log(x.length);
   chrome.runtime.sendNativeMessage(
-    'by.styx.run', { srcUrl: info.srcUrl, bin: "pinta" }
+    'by.styx.run', { data: x, bin: 'pinta' }
   );
 }
 
-chrome.contextMenus.create({"title": "Open with Pinta", "contexts":["image"], "onclick": sendMsg});
+function msgReceived(info, tab) {
+  getImg(info.srcUrl);
+}
+
+chrome.contextMenus.create({'title': 'Open with Pinta', 'contexts': ['image'], 'onclick': msgReceived});
